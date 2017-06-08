4643fafd0dafd2c57a4ec9664c06849ca6cd1a9e
Takashiro <takashiro@qq.com>
2017-06-08 22:18:42 +0800

    Add structs

---
+++ src/gamelogic/structs.cpp
@@ -0,0 +1,166 @@
+/***********************************************************************
+H5Sanguosha Server
+Copyright (C) 2016-2017  Kazuichi Takashiro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+takashiro@qq.com
+************************************************************************/
+
+#include "structs.h"
+
+#include "Card.h"
+
+KA_USING(Json)
+KA_USING(JsonObject)
+KA_USING(JsonArray)
+
+CardsMoveStruct::Area::Area()
+	: type(CardAreaType::Unknown)
+	, direction(CardMoveDirection::Undefined)
+	, owner(nullptr)
+{
+}
+
+Json CardsMoveStruct::Area::toJson() const
+{
+	JsonObject data;
+	data["type"] = static_cast<int>(type);
+	data["direction"] = direction;
+	data["ownerId"] = ownerId;
+	data["name"] = name;
+	return data;
+}
+
+CardsMoveStruct::CardsMoveStruct()
+	: isOpen(false)
+	, isLastHandCard(false)
+	, origin(nullptr)
+{
+}
+
+CardsMoveStruct::~CardsMoveStruct()
+{
+	if (origin)
+		delete origin;
+}
+
+bool CardsMoveStruct::isRelevant(const ServerPlayer *player) const
+{
+	return player == nullptr || player == from.owner || (player == to.owner && to.type != CardAreaType::Special);
+}
+
+Json CardsMoveStruct::toJson(bool open) const
+{
+	JsonObject data;
+	data["from"] = from.toJson();
+	data["to"] = to.toJson();
+
+	if (isOpen || open) {
+		JsonArray card_data;
+		for (const Card *card : cards)
+			card_data.push_back(card->id());
+		data["cards"] = card_data;
+	} else {
+		data["cards"] = cards.size();
+	}
+
+	data["isOpen"] = isOpen;
+	data["isLastHandCard"] = isLastHandCard;
+	return data;
+}
+
+CardUseStruct::CardUseStruct()
+	: from(nullptr)
+	, card(nullptr)
+	, target(nullptr)
+	, isNullified(false)
+	, addHistory(true)
+	, isHandCard(true)
+{
+}
+
+CardEffectStruct::CardEffectStruct(CardUseStruct &use)
+	: use(use)
+	, from(use.from)
+	, to(nullptr)
+{
+}
+
+bool CardEffectStruct::isNullified() const
+{
+	if (use.isNullified) {
+		return true;
+	}
+
+	if (to) {
+		for (ServerPlayer *player : use.nullifiedList) {
+			if (player == to) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+DamageStruct::DamageStruct()
+	: from(nullptr)
+	, to(nullptr)
+	, card(nullptr)
+	, damage(1)
+	, nature(Normal)
+	, chain(false)
+	, transfer(false)
+	, byUser(true)
+	, prevented(false)
+{
+}
+
+RecoverStruct::RecoverStruct()
+	: from(nullptr)
+	, to(nullptr)
+	, recover(1)
+	, card(nullptr)
+{
+}
+
+CardResponseStruct::CardResponseStruct()
+	: from(nullptr)
+	, to(nullptr)
+	, card(nullptr)
+	, target(nullptr)
+{
+}
+
+JudgeStruct::JudgeStruct(const std::string &pattern)
+	: pattern(pattern)
+	, who(nullptr)
+	, card(nullptr)
+	, matched(false)
+{
+}
+
+DeathStruct::DeathStruct()
+	: who(nullptr)
+	, damage(nullptr)
+{
+}
+
+SkillStruct::SkillStruct()
+	: owner(nullptr)
+	, skill(nullptr)
+	, area(SkillAreaType::Unknown)
+{
+}
---
+++ src/gamelogic/structs.h
@@ -0,0 +1,167 @@
+/***********************************************************************
+H5Sanguosha Server
+Copyright (C) 2016-2017  Kazuichi Takashiro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+takashiro@qq.com
+************************************************************************/
+
+#pragma once
+
+#include "common.h"
+
+#include "CardAreaType.h"
+#include "CardMoveDirection.h"
+#include "PlayerPhase.h"
+#include "SkillAreaType.h"
+#include "CardPattern.h"
+
+#include <vector>
+#include <string>
+#include <Json.h>
+
+class Card;
+class ServerPlayer;
+class Skill;
+
+struct CardsMoveStruct
+{
+	struct Area
+	{
+		CardAreaType type;
+		CardMoveDirection direction;
+		ServerPlayer *owner;
+		uint ownerId;
+		std::string name;
+
+		Area();
+		KA_IMPORT Json toJson() const;
+	};
+
+	Area from;
+	Area to;
+	std::vector<Card *> cards;
+	bool isOpen;
+	bool isLastHandCard;
+	CardsMoveStruct *origin;
+
+	CardsMoveStruct();
+	~CardsMoveStruct();
+
+	bool isRelevant(const ServerPlayer *player) const;
+	KA_IMPORT Json toJson(bool open = false) const;
+};
+
+struct PhaseChangeStruct
+{
+	PlayerPhase from;
+	PlayerPhase to;
+};
+
+struct CardUseStruct
+{
+	ServerPlayer *from;
+	std::vector<ServerPlayer *> to;
+	Card *card;
+	Card *target;
+	std::vector<ServerPlayer *> nullifiedList;
+	bool isNullified;
+	bool addHistory;
+	bool isHandCard;
+	KA_IMPORT Json extra;
+
+	CardUseStruct();
+};
+
+struct CardEffectStruct
+{
+	CardUseStruct &use;
+	ServerPlayer *&from;
+	ServerPlayer *to;
+
+	CardEffectStruct(CardUseStruct &use);
+	bool isNullified() const;
+};
+
+struct DamageStruct
+{
+	enum Nature
+	{
+		Normal,
+		Fire,
+		Thunder
+	};
+
+	ServerPlayer *from;
+	ServerPlayer *to;
+	Card *card;
+	int damage;
+	Nature nature;
+	bool chain;
+	bool transfer;
+	bool byUser;
+	std::string reason;
+	std::string transferReason;
+	bool prevented;
+
+	DamageStruct();
+};
+
+struct RecoverStruct
+{
+	ServerPlayer *from;
+	ServerPlayer *to;
+	int recover;
+	Card *card;
+
+	RecoverStruct();
+};
+
+struct CardResponseStruct
+{
+	ServerPlayer *from;
+	ServerPlayer *to;
+	Card *card;
+	Card *target;
+
+	CardResponseStruct();
+};
+
+struct JudgeStruct
+{
+	CardPattern pattern;
+	ServerPlayer *who;
+	Card *card;
+	bool matched;
+
+	JudgeStruct(const std::string &pattern);
+};
+
+struct DeathStruct
+{
+	ServerPlayer *who;
+	DamageStruct *damage;
+
+	DeathStruct();
+};
+
+struct SkillStruct
+{
+	ServerPlayer *owner;
+	const Skill *skill;
+	SkillAreaType area;
+
+	SkillStruct();
+};
